f5e76e87551f0b2ad81ef3c9babfa7d66ac3d1f2
Staci Southerland <stacisoutherland24@gmail.com>
2022-09-03 12:05:04 -0500

    week 4 assignment

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -1,16 +1,23 @@
-$(function() {
-     $(".carousel").carousel( { interval: 2000} );    
-     $("carouselButton").click(function(){
-          if ($("carouselButton").children("i").hasClass("fa-pause")) {
-               $(".carousel").carousel("pause");
-               $("#carouselButton").children("i").removeClass("fa-pause");
-               $("#carouselButton").children("i").addClass("fa-play");
-          }else {
-               $(".carousel").carousel("cycle");
-               $("#carouselButton").children("i").removeClass("fa-play");
-               $("#carouselButton").children("i").addClass("fa-pause");
-          }
-     });
-     
-     });
+$(function () {
+  $(".carousel").carousel({ interval: 2000 });
+  $("carouselButton").on("click", function () {
+    if ($("carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle");
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
+  });
 });
+
+$("#loginButton").on("click", function () {
+  $("#loginModal").modal("show");
+});
+$("#reserveButton").on("click", function () {
+  $("#reserveModal").modal("show");
+});
+//$('#loginButton').on('click', ()
+console.log("working");
